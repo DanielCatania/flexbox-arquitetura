de24971591680bf501a78005c4a95e69c987c2f5
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-25 14:48:32 -0300

    feat: update global style

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <body>
+      <body className="bg-neutral-50">
         <Main />
         <NextScript />
       </body>
