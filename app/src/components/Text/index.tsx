33e6be24c22ccde229824b890ce5fdf33c5fdaed
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-25 19:27:33 -0300

    style: updating text size

--- app/src/components/Text/index.tsx
+++ app/src/components/Text/index.tsx
@@ -10,42 +10,42 @@ export default function Text({ size, children, className }: IText) {
   let style = "";
   switch (size) {
     case "ut":
-      style = "lg:text-ut lg:font-ut text-xl font-xs";
+      style = "lg:text-ut lg:font-ut md:text-2xl md:font-xs text-xl font-xs";
       return (
         <h1 className={`${style} ${className}`}>
           {children}
         </h1>
       );
     case "2xl":
-      style = "md:text-2xl text-l font-xs";
+      style = "md:text-2xl text-xl font-xs";
       return (
         <h2 className={`${style} ${className}`}>
           {children}
         </h2>
       );
     case "xl":
-      style = "md:text-xl md:font-xs text-sl font-s";
+      style = "md:text-xl text-l font-xs";
       return (
         <h3 className={`${style} ${className}`}>
           {children}
         </h3>
       );
     case "l":
-      style = "md:text-l md:font-xs text-r font-r";
+      style = "md:text-l md:font-xs text-sl font-s";
       return (
         <h4 className={`${style} ${className}`}>
           {children}
         </h4>
       );
     case "sl":
-      style = "md:text-sl md:font-s text-s font-r";
+      style = "md:text-sl md:font-s text-r font-r";
       return (
         <h5 className={`${style} ${className}`}>
           {children}
         </h5>
       );
     case "r":
-      style = "md:text-r md:font-r text-leg font-w";
+      style = "md:text-r md:font-r text-s font-r";
       return (
         <p className={`${style} ${className}`}>
           {children}
