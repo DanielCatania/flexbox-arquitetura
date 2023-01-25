781d4cb82cc397c4e76717b2ddda3fd433a61d32
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-25 11:18:34 -0300

    style: vertically aligning links

--- app/src/components/Links/index.tsx
+++ app/src/components/Links/index.tsx
@@ -1,12 +1,17 @@
 import Link from "./Link";
 
-interface ILinks {
+export interface ILinks {
+  direction?: "h" | "v"; // h - horizontal | v - vertical
   className?: string;
 }
 
-export default function Links({ className }: ILinks) {
+export default function Links({ direction = "h", className }: ILinks) {
+  const horizontalStyle = "font-w text-leg sm:font-s sm:text-sl flex items-center gap-8 p-4 text-neutral-050";
+  const verticalStyle = "flex flex-col gap-4 p-4 font-s text-sl text-neutral-050";
+  const style = direction === "h" ? horizontalStyle : verticalStyle;
+
   return (
-    <ul className={`font-w text-leg sm:font-s sm:text-sl flex items-center gap-8 p-4 text-neutral-050 ${className}`}>
+    <ul className={`${style} ${className}`}>
       <Link href="/" text="Home" />
       <Link href="/about" text="Quem Somos" />
       <Link href="/projects" text="Projetos" />
--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -9,17 +9,25 @@ export default function Menu() {
   return (
     <>
       <Links className="hidden sm:flex" />
-      <button type="button" onClick={() => setMenuActive(true)}>
-        <Bars3Icon className="text-neutral-050 sm:hidden h-6" />
+      <button 
+        type="button" 
+        onClick={() => setMenuActive(true)}
+        className="sm:hidden"
+      >
+        <Bars3Icon className="text-neutral-050 h-6" />
       </button>
       <div
         className={`fixed top-0 left-0 right-0 h-full bg-neutral-op-050 flex justify-end ${menuDisplay}`}
       >
-        <nav className="bg-neutral-150 w-2/3 h-full px-8 py-4 flex flex-col items-end">
-          <button type="button" onClick={() => setMenuActive(false)}>
+        <nav className="bg-neutral-150 w-2/3 h-full px-8 py-4 flex flex-col">
+          <button 
+            type="button" 
+            onClick={() => setMenuActive(false)}
+            className="w-full flex justify-end"
+          >
             <XMarkIcon className="text-neutral-050 h-8" />
           </button>
-          <Links />
+          <Links direction="v"/>
         </nav>
       </div>
     </>
