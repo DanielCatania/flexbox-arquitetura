8403f7c412445eb437ed774845a334daf887aca7
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-26 19:06:39 -0300

    style: pinning the header

--- app/src/patterns/Header/index.tsx
+++ app/src/patterns/Header/index.tsx
@@ -3,7 +3,9 @@ import Menu from "@/components/Menu";
 
 export default function Header() {
   return (
-    <header className="px-4 py-2 lg:px-8 lg:py-4 bg-neutral-150 flex justify-between items-center">
+    <header 
+      className="fixed right-0 left-0 top-0 px-4 py-2 lg:px-8 lg:py-4 bg-neutral-150 flex justify-between items-center"
+    >
       <Logo />
       <Menu />
     </header>
--- app/src/screens/HomeScreen/index.tsx
+++ app/src/screens/HomeScreen/index.tsx
@@ -6,12 +6,14 @@ import Horizon from "./Horizon";
 
 export default function Home() {
   return (
-    <>
+    <main 
+      className="pt-[56px] sm:pt-[78px] md:pt-[112px]"
+    >
       <Header />
       <Banner />
       <About />
       <Horizon />
       <Footer />
-    </>
+    </main>
   );
 }
