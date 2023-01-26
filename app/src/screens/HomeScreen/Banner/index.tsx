90cfe892c9d60ee48b6b9954e37ff1f43698c41e
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-26 19:23:34 -0300

    style: adjusting banner size

--- app/src/screens/HomeScreen/Banner/index.tsx
+++ app/src/screens/HomeScreen/Banner/index.tsx
@@ -3,9 +3,10 @@ import Text from "@/components/Text";
 export default function  Banner() {
   return (
     <section 
-      className="bg-banner bg-no-repeat bg-contain bg-right-top h-1/3 lg:h-3/4 md:px-8 px-4 flex items-center"
+      className="bg-banner bg-no-repeat bg-contain bg-right-top h-1/3 md:h-5/6 md:px-8 px-4 flex items-center"
     >
-      <Text size="ut" className="text-primary-100 w-4/5 md:w-5/6">
+      <Text size="ut" className="text-primary-100 w-4/5 md:w-3/4 lg:w-5/6"> 
+      {/*  */}
         A agência que <span className="text-secondary-100">resolve</span> sua dor de cabeça.
       </Text>
     </section>
