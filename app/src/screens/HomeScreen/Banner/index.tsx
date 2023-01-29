786a9ff2c820f2f9027d486e65379b456cd25422
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-01-29 10:51:38 -0300

    style: adding vertical padding on banner

--- app/src/screens/HomeScreen/Banner/index.tsx
+++ app/src/screens/HomeScreen/Banner/index.tsx
@@ -3,10 +3,9 @@ import Text from "@/components/Text";
 export default function  Banner() {
   return (
     <section 
-      className="bg-banner bg-no-repeat bg-contain bg-right-top h-1/3 md:h-5/6 md:px-8 px-4 flex items-center"
+      className="bg-banner bg-no-repeat bg-contain bg-right-top py-4 sm:py-16 md:py-24 md:px-8 px-4 flex items-center"
     >
       <Text size="ut" className="text-primary-100 w-4/5 md:w-3/4 lg:w-5/6"> 
-      {/*  */}
         A agência que <span className="text-secondary-100">resolve</span> sua dor de cabeça.
       </Text>
     </section>
